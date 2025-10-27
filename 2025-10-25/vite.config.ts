e0186a1255eb5767f7ac1ac3b39d7780389f0f4b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-27 15:03:41 +0900

    lint

--- 2025-10-25/vite.config.ts
+++ 2025-10-25/vite.config.ts
@@ -13,8 +13,14 @@ export default defineConfig({
         // https://vercel.com/docs/environment-variables/system-environment-variables#VERCEL_URL
         // https://github.com/vercel/next.js/blob/498349c375e2602f526f64e8366992066cfa872c/packages/next/src/lib/metadata/resolvers/resolve-url.ts#L10-L55
         if (process.env.VERCEL_URL) {
-          const origin = process.env.VERCEL_ENV === 'preview' ? (process.env.VERCEL_BRANCH_URL || process.env.VERCEL_URL) : process.env.VERCEL_PROJECT_PRODUCTION_URL;
-          const ogImageUrl = new URL("./og-image.png", new URL(resolvedConfig.base, `https://${origin}`)).href;
+          const origin =
+            process.env.VERCEL_ENV === "preview"
+              ? process.env.VERCEL_BRANCH_URL || process.env.VERCEL_URL
+              : process.env.VERCEL_PROJECT_PRODUCTION_URL;
+          const ogImageUrl = new URL(
+            "./og-image.png",
+            new URL(resolvedConfig.base, `https://${origin}`),
+          ).href;
           return [
             {
               tag: "meta",
